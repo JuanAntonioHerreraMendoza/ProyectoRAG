57d4ae8e9e42205c3bbe4b29d76b30a9d14c20ac
Antonio <17161136@itoaxaca.edu.mx>
2023-03-01 17:53:39 -0600

    Imagen del menu

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,24 @@
 import React, { useContext } from "react";
-import { Button, Text, View } from "react-native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import {
+  Button,
+  Text,
+  View,
+  ImageBackground,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+} from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 import { AuthContext } from "../context/AuthContext";
 import ReporteForm from "./ReporteForm";
 import Camara from "../components/Camara";
 import Reportes from "./Reportes";
 import ReporteDetail from "../components/Reportes/ReporteDetail";
+import menuImage from "../components/menuImage";
 
 function Home({ navigation }) {
   const { userInfo, isLoading, logout } = useContext(AuthContext);
@@ -22,29 +34,38 @@ function Home({ navigation }) {
 const Drawer = createDrawerNavigator();
 
 export default function HomeScreen() {
+  const { userInfo, logout } = useContext(AuthContext);
+
   return (
-    <Drawer.Navigator initialRouteName="Home">
-      <Drawer.Screen name="Home" component={Home} />
+    <Drawer.Navigator
+      initialRouteName="Inicio"
+      screenOptions={{
+        drawerStyle: { backgroundColor: "#222f3e" },
+        drawerLabelStyle: { color: "white" },
+      }}
+      drawerContent={(props)=>menuImage(props,logout,userInfo)}
+    >
+      <Drawer.Screen name="Inicio" component={Home} options={{ headerTitle: "Inicio",headerTintColor:"white",headerStyle:{backgroundColor:"#222f3e"},headerTitleStyle:{color:"white"}}}/>
       <Drawer.Screen
         name="Reporte"
         component={ReporteForm}
-        options={{ headerTitle: "Crear reporte" }}
+        options={{ headerTitle: "Crear reporte",headerTintColor:"white",headerStyle:{backgroundColor:"#222f3e"},headerTitleStyle:{color:"white"}}}
       />
       <Drawer.Screen
-        name="Camara"
-        component={Camara}
+        name="Reportes"
+        component={Reportes}
+        options={{ headerTitle: "Mis reportes", headerTitleAlign: "center",headerTintColor:"white",headerStyle:{backgroundColor:"#222f3e"},headerTitleStyle:{color:"white"}}}
+      />
+      <Drawer.Screen
+        name="ReporteDetail"
+        component={ReporteDetail}
         options={{
           drawerItemStyle: { height: 0 },
         }}
       />
       <Drawer.Screen
-        name="Reportes"
-        component={Reportes}
-        options={{ headerTitle: "Mis reportes", headerTitleAlign: "center" }}
-      />
-      <Drawer.Screen
-        name="ReporteDetail"
-        component={ReporteDetail}
+        name="Camara"
+        component={Camara}
         options={{
           drawerItemStyle: { height: 0 },
         }}
